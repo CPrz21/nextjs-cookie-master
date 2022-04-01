e338cdf5f08cc5bc153a8c45052d49eca8599969
CPrz21 <faselona@gmail.com>
2022-04-01 17:45:07 -0600

    add prettier and eslint configs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <div>cookie master</div>;
-};
+  return <div>cookie master</div>
+}
 
-export default Home;
+export default Home
